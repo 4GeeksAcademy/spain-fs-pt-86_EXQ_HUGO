1bc9b8a49ff69541441550e355fc56b75a70ec64
Hvaxquez <hugoferney54@gmail.com>
2024-11-12 10:58:40 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,60 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const WHO_LIST = new Array("A two", "A lonely", "A lonely", "A lonely");
+  const ACTION_LIST = new Array(
+    "headed pinecone",
+    "jogger kiked",
+    "merchant kiked",
+    "comedian burned"
+  );
+  const WHAT_LIST = new Array(
+    "yellet at my sandwich",
+    "my sandwich",
+    "my car near",
+    "my car in front"
+  );
+  const WHEN_LIST = new Array(
+    "in my house",
+    "on the street",
+    "the toilet",
+    "of the office"
+  );
+
+  function generadorDeExcusas() {
+    const who = obtenerWhoAleatoria();
+    const action = obtenerActionAleatoria();
+    const what = obtenerWhatAleatoria();
+    const when = obtenerWhenAleatoria();
+    const excusa = generarExcusa(who, action, what, when);
+    return excusa;
+  }
+
+  function obtenerElementoAleatorio(list) {
+    const listPosition = Math.floor(Math.random() * list.length);
+    return list[listPosition];
+  }
+
+  function obtenerWhoAleatoria() {
+    return obtenerElementoAleatorio(WHO_LIST);
+  }
+
+  function obtenerActionAleatoria() {
+    return obtenerElementoAleatorio(ACTION_LIST);
+  }
+
+  function obtenerWhatAleatoria() {
+    return obtenerElementoAleatorio(WHAT_LIST);
+  }
+
+  function obtenerWhenAleatoria() {
+    return obtenerElementoAleatorio(WHEN_LIST);
+  }
+
+  function generarExcusa(who, action, what, when) {
+    return `${who} ${action} ${what} ${when}.`;
+  }
+
+  document.getElementById("excuses").innerHTML = generadorDeExcusas();
+  //console.log("Hello Rigo from the console!");
 };
